983bbb15afd97372e8fb64af7e6c9b86c670bf6f
Owen Jones <jones@biology.sdu.dk>
2020-01-14 22:19:20 +0100

    Modified climwin run script to write results directly out to a csv
    Modified climwin run script to write results directly out to a csv. This avoids the problem of the computer crashing and losing the data.

--- R/climwin1_run_precip.R
+++ R/climwin1_run_precip.R
@@ -1,26 +1,12 @@
 #climwin_run_precip.R
 
-
 # precipitation
-ppt_list <- list()
-
 for (i in 1:nrow(spp_summary)) {
   dat <- filter(comp_sub, SpeciesAuthor == spp_summary$SpeciesAuthor[i])
-  ppt_list[[i]] <- climwin_summary(dat, var = "ppt_scale")
+  climwinOut_ppt <- climwin_summary(dat, var = "ppt_scale")
   
-  if(i == 1) cat(paste0(i,"/",nrow(spp_summary), " - precipitation \n"),file="/Users/jones/Dropbox/!Inbox/temp_PrecipProgress.txt",append = FALSE)
-  if(i > 1) cat(paste0(i,"/",nrow(spp_summary), " - precipitation \n"),file="/Users/jones/Dropbox/!Inbox/temp_PrecipProgress.txt",append = TRUE)
+  if(i == 1) write(paste(as.character(names(climwinOut_ppt)),collapse=", "), file="output/climwin_ppt.csv", append=FALSE)
+  if(i == 1) write(paste(as.data.frame(climwinOut_ppt)[1,],collapse=", "), file="output/climwin_ppt.csv", append=TRUE)
   
-}
-
-
-# # convert p-values to character
-# for (i in 1:nrow(spp_summary)) {
-#   tmp_list[[i]]$p_a <- as.character(tmp_list[[i]]$p_a)
-#   ppt_list[[i]]$p_a <- as.character(ppt_list[[i]]$p_a)
-# }
-
-
-### write results to file
-ppt_out <- bind_rows(ppt_list)
-write.csv(ppt_out, "output/climwin_ppt.csv", row.names = FALSE)
+  if(i > 1) write(paste(as.data.frame(climwinOut_ppt)[1,],collapse=", "), file="output/climwin_ppt.csv", append=TRUE)
+  }
--- R/climwin1_run_temp.R
+++ R/climwin1_run_temp.R
@@ -1,16 +1,19 @@
 #climwin1_run_temp
 
 # temperature
-tmp_list <- list()
-
 for (i in 1:nrow(spp_summary)) {
   dat <- filter(comp_sub, SpeciesAuthor == spp_summary$SpeciesAuthor[i])
-  tmp_list[[i]] <- climwin_summary(dat, var = "tmp_scale")
+  climwinOut_tmp <- climwin_summary(dat, var = "tmp_scale")
   
-  if(i == 1) cat(paste0(i,"/",nrow(spp_summary), " - temperature \n"),file="/Users/jones/Dropbox/!Inbox/temp_TemperatureProgress.txt",append = FALSE)
-  if(i > 1) cat(paste0(i,"/",nrow(spp_summary), " - temperature \n"),file="/Users/jones/Dropbox/!Inbox/temp_TemperatureProgress.txt",append = TRUE)
+  if(i == 1) write(paste(as.character(names(climwinOut_tmp)),collapse=", "), file="output/climwin_tmp.csv", append=FALSE)
+  if(i == 1) write(paste(as.data.frame(climwinOut_tmp)[1,],collapse=", "), file="output/climwin_tmp.csv", append=TRUE)
+  
+  if(i > 1) write(paste(as.data.frame(climwinOut_tmp)[1,],collapse=", "), file="output/climwin_tmp.csv", append=TRUE)
 }
+
+
 
 ### write results to file
 tmp_out <- bind_rows(tmp_list)
 write.csv(tmp_out, "output/climwin_tmp.csv", row.names = FALSE)
+
