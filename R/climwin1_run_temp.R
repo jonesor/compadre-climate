3f98e4fabf25f72c6fd14f23362e4ac7818e9ca7
Owen Jones <jonesor@gmail.com>
2020-01-17 10:12:32 +0100

    Update climwin1_run_temp.R

--- R/climwin1_run_temp.R
+++ R/climwin1_run_temp.R
@@ -10,10 +10,3 @@ for (i in 1:nrow(spp_summary)) {
   
   if(i > 1) write(paste(as.data.frame(climwinOut_tmp)[1,],collapse=", "), file="output/climwin_tmp.csv", append=TRUE)
 }
-
-
-
-### write results to file
-tmp_out <- bind_rows(tmp_list)
-write.csv(tmp_out, "output/climwin_tmp.csv", row.names = FALSE)
-
